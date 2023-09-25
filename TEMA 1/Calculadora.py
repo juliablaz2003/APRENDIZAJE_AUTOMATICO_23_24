a04a3bd8f37f0ec0ba8c08666ee5858689d9b4b2
juliablaz2003 <144936319+juliablaz2003@users.noreply.github.com>
2023-09-25 16:05:35 +0200

    Update Calculadora.py

--- TEMA 1/Calculadora.py
+++ TEMA 1/Calculadora.py
@@ -1,5 +1,5 @@
-class Calculadora:
-    def __init__(self, x=0, y=0):
+class Calculadora:  # se define la clase calculadora
+    def __init__(self, x=0, y=0): # si no me pasas ningun valor coge automaticamente 0 para x e y, este es el constructor
         self._x = x
         self._y = y
         
@@ -16,8 +16,8 @@ class Calculadora:
     
     def get_y(self):
         return self._y
-        
-    # Métodos para las operaciones básicas
+
+# Métodos para las operaciones básicas
     def suma(self):
         return self._x + self._y
     
@@ -35,17 +35,18 @@ class Calculadora:
 
 if __name__ == "__main__":
     # Ejemplo de uso
-    calc = Calculadora(10, 5)  # Se crea un objeto calculadora con x=10 y y=5
+    calc = Calculadora(10, 5)  # Se crea un objeto calculadora con x=10 y y=5, llamo a la clase calculadora
     
-    print("x:", calc.get_x())  # Se imprime el valor de x
-    print("y:", calc.get_y())  # Se imprime el valor de y
+    print("x:", calc.get_x())  # Se imprime el valor de x=10
+    print("y:", calc.get_y())  # Se imprime el valor de y=5
 
+    # Tambien se puede llamar directamente al atributo
     print("x:", calc._x)  # Se imprime el valor de x
     print("y:", calc._y)  # Se imprime el valor de y
     
-    print("Suma:", calc.suma())  # Se imprime el resultado de la suma
-    print("Resta:", calc.resta())  # Se imprime el resultado de la resta
-    print("Multiplicación:", calc.multiplicacion())  # Se imprime el resultado de la multiplicación
+    print("Suma:", calc.suma())  # Se imprime el resultado de la suma 10+5=15
+    print("Resta:", calc.resta())  # Se imprime el resultado de la resta 10-5=5
+    print("Multiplicación:", calc.multiplicacion())  # Se imprime el resultado de la multiplicación 10*5=50
     print("División:", calc.division())  # Se imprime el resultado de la división
     
     # Cambiar valores de x e y usando los métodos Setter
@@ -55,4 +56,4 @@ if __name__ == "__main__":
     print("\nValores Actualizados")
     print("x:", calc.get_x())  # Se imprime el nuevo valor de x
     print("y:", calc.get_y())  # Se imprime el nuevo valor de y
-    print("División con nuevos valores:", calc.division())  # Se intenta realizar una división por cero
+    print("División con nuevos valores:", calc.division())  # Se intenintenta realizar una división por cero
